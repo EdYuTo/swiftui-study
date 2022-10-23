50a03d7c11f713297c7f3f231d4259ca7263f2ab
EdYuTo <edson.yudi.toma@gmail.com>
2022-10-23 17:01:07 -0300

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Edson Yudi Toma on 23/10/22.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
