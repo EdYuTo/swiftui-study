8bc4b37a5f7fe3d4a753e3a98893e3a21547976d
EdYuTo <edson.yudi.toma@gmail.com>
2023-01-22 14:55:47 -0300

    Initial Commit

---
+++ frameworks/frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  frameworksApp.swift
+//  frameworks
+//
+//  Created by Edson Yudi Toma on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
