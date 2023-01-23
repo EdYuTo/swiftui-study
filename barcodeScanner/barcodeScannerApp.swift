a416a231fd19b69075a83ee971f1bf559f8f3799
EdYuTo <edson.yudi.toma@gmail.com>
2023-01-23 19:50:54 -0300

    Initial Commit

---
+++ barcodeScanner/barcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcodeScannerApp.swift
+//  barcodeScanner
+//
+//  Created by Edson Yudi Toma on 23/01/23.
+//
+
+import SwiftUI
+
+@main
+struct barcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
