49531e2072582abe95521d6056389a5fce3193fb
EdYuTo <edson.yudi.toma@gmail.com>
2023-02-12 14:05:08 -0300

    Initial Commit

---
+++ MealsApp/MealsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsAppApp.swift
+//  MealsApp
+//
+//  Created by Edson Yudi Toma on 12/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MealsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
